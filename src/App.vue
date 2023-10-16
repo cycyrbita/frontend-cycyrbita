451647a0e8bb51701477776010561dccf32778ff
Никита <35835844+nikita-developer@users.noreply.github.com>
2023-10-16 18:28:35 +0300

    переход на axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+	<button @click="refresh">refresh</button>
 	<router-view></router-view>
 </template>
 
+<script setup>
+import axios from 'axios';
+
+const refresh = () => {
+axios.get('http://localhost:5000/api/refresh', { withCredentials: true })
+	.then((res) => {
+		console.log(res)
+	})
+}
+</script>
