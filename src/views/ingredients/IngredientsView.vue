6a05f9a429cb0b5ea489265f3d1352a076473051
Никита <35835844+nikita-developer@users.noreply.github.com>
2023-11-07 18:23:38 +0300

    ингрейдиенты

--- src/views/ingredients/IngredientsView.vue
+++ src/views/ingredients/IngredientsView.vue
@@ -1,23 +1,47 @@
 <template>
-	<form @submit.prevent="send">
-		<input
-			type="file"
-			name="ingredients"
-			multiple
-			v-on:change="handleFileUpload($event)"
-		/>
-		<button type="submit">Отправить</button>
-	</form>
+	<div>
+		<input v-model="country" type="text" placeholder="Страна">
+		<input v-model="title" type="text" placeholder="Заголовок">
+		<textarea v-model="description" type="text" placeholder="Описание"></textarea>
+		<input v-model="tag" type="text" placeholder="Тег">
+		<input v-model="theme" type="text" placeholder="Тема">
+		<input type="file" multiple @change="handleFileUpload" ref="uploadIngredients">
+		<button @click="send" type="submit">Отправить</button>
+	</div>
 </template>
 
 <script setup>
 import useFetch from '@/composables/useFetch'
+import {ref} from 'vue'
 let formData = new FormData()
 
-const handleFileUpload = (e) => {
-	for (let key in e.target.files) formData.append('ingredients', e.target.files[key])
+const ingredients = ref([])
+const tags = ref([])
+
+const country = ref()
+const title = ref()
+const description = ref()
+const tag = ref()
+const theme = ref()
+const uploadIngredients = ref(null)
+
+// запихиваем картинки в массив и добавляем в formData
+const handleFileUpload = () => {
+	for (let key in uploadIngredients.value.files) formData.append('ingredientsImages', uploadIngredients.value.files[key])
 }
 
+// создаем ингредиент
+const addIngredient = () => {
+	ingredients.value.push({
+		country: country.value,
+		title: title.value,
+		description: description.value,
+		tags: tags.value,
+		theme: theme.value,
+	})
+}
+
+// метод отправки
 const send = async () => {
 	try {
 		const headers = {
@@ -26,10 +50,18 @@ const send = async () => {
 			credentials: 'include',
 		}
 
+		addIngredient()
+		formData.append('ingredients', JSON.stringify(ingredients.value))
+
 		const res = await useFetch.post('ingredients/create', null, headers)
 		const json = await res.json()
 
 		formData.delete('ingredients')
+		formData.delete('ingredientsImages')
+		ingredients.value = []
+		tags.value = []
+
+		uploadIngredients.value.value = ''
 
 		console.log(json)
 	} catch (e) {
@@ -37,3 +69,9 @@ const send = async () => {
 	}
 }
 </script>
+
+<style scoped lang="scss">
+input {
+	display: block;
+}
+</style>
