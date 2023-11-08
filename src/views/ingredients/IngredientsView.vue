24b9af4404301f255e41f55a1a9f2e68110827ad
Никита <35835844+nikita-developer@users.noreply.github.com>
2023-11-08 18:43:47 +0300

    ингредиенты

--- src/views/ingredients/IngredientsView.vue
+++ src/views/ingredients/IngredientsView.vue
@@ -1,13 +1,61 @@
 <template>
 	<div>
-		<input v-model="country" type="text" placeholder="Страна">
-		<input v-model="title" type="text" placeholder="Заголовок">
-		<textarea v-model="description" type="text" placeholder="Описание"></textarea>
-		<input v-model="tag" type="text" placeholder="Тег">
-		<input v-model="theme" type="text" placeholder="Тема">
-		<input type="file" multiple @change="handleFileUpload" ref="uploadIngredients">
-		<button @click="send" type="submit">Отправить</button>
+		<label v-for="(country, index) in countrys">
+			<input v-model="country.country" type="text" placeholder="Язык">
+			<button @click="countrys.splice(index, 1)">x</button>
+		</label>
+		<button @click="addCountry">Добавить язык</button>
+		<hr>
+
+		<label v-for="(title, index) in titles">
+			<input v-model="title.title" type="text" placeholder="Название ингредиента">
+			<select v-model="title.country">
+				<option selected value="">Выберите язык</option>
+				<option v-for="country in countrys" :value="country.country">{{country.country}}</option>
+			</select>
+			<button @click="titles.splice(index, 1)">x</button>
+		</label>
+		<button @click="addTitle">Добавить название</button>
+		<hr>
+
+		<label v-for="(theme, index) in themes">
+			<input v-model="theme.theme" type="text" placeholder="Название темы">
+			<button @click="themes.splice(index, 1)">x</button>
+		</label>
+		<button @click="addTheme">Добавить тему</button>
+		<hr>
+
+		<label v-for="(description, index) in descriptions">
+			<textarea v-model="description.description" type="text" placeholder="Описание"></textarea>
+			<select v-model="description.country">
+				<option selected value="">Выберите язык</option>
+				<option v-for="country in countrys" :value="country.country">{{country.country}}</option>
+			</select>
+			<select v-model="description.themes" multiple="multiple">
+				<option selected value="">Выберите тему</option>
+				<option v-for="theme in themes" :value="theme.theme">{{theme.theme}}</option>
+			</select>
+			<button @click="descriptions.splice(index, 1)">x</button>
+		</label>
+		<button @click="addDescription">Добавить описание</button>
+		<hr>
+
+		<label v-for="(tag, index) in tags">
+			<input v-model="tag.tag" type="text" placeholder="Название тега">
+			<select v-model="tag.themes" multiple="multiple">
+				<option selected value="">Выберите тему</option>
+				<option v-for="theme in themes" :value="theme.theme">{{theme.theme}}</option>
+			</select>
+			<button @click="tags.splice(index, 1)">x</button>
+		</label>
+		<button @click="addTag">Добавить тег</button>
+		<hr>
+
 	</div>
+	<label>
+		<input type="file" multiple @change="handleFileUpload" ref="uploadImages">
+	</label>
+	<button @click="send" type="submit">Отправить</button>
 </template>
 
 <script setup>
@@ -15,30 +63,25 @@ import useFetch from '@/composables/useFetch'
 import {ref} from 'vue'
 let formData = new FormData()
 
-const ingredients = ref([])
-const tags = ref([])
+const addTitle = () => titles.value.push({title: '', country: ''})
+const addDescription = () => descriptions.value.push({description: '', country: '', themes: []})
+const addTag = () => tags.value.push({tag: '', themes: []})
+const addTheme = () => themes.value.push({theme: ''})
+const addCountry = () => countrys.value.push({country: ''})
 
-const country = ref()
-const title = ref()
-const description = ref()
-const tag = ref()
-const theme = ref()
-const uploadIngredients = ref(null)
+const titles = ref([])
+const countrys = ref([])
+const descriptions = ref([])
+const tags = ref([])
+const themes = ref([])
+
+const ingredients = ref()
+
+const uploadImages = ref(null)
 
 // запихиваем картинки в массив и добавляем в formData
 const handleFileUpload = () => {
-	for (let key in uploadIngredients.value.files) formData.append('ingredientsImages', uploadIngredients.value.files[key])
-}
-
-// создаем ингредиент
-const addIngredient = () => {
-	ingredients.value.push({
-		country: country.value,
-		title: title.value,
-		description: description.value,
-		tags: tags.value,
-		theme: theme.value,
-	})
+	for (let key in uploadImages.value.files) formData.append('ingredientsImages', uploadImages.value.files[key])
 }
 
 // метод отправки
@@ -50,18 +93,20 @@ const send = async () => {
 			credentials: 'include',
 		}
 
-		addIngredient()
+		ingredients.value = {
+			titles: titles.value,
+			countrys: countrys.value,
+			descriptions: descriptions.value,
+			tags: tags.value,
+			themes: themes.value
+		}
+
 		formData.append('ingredients', JSON.stringify(ingredients.value))
 
 		const res = await useFetch.post('ingredients/create', null, headers)
 		const json = await res.json()
 
 		formData.delete('ingredients')
-		formData.delete('ingredientsImages')
-		ingredients.value = []
-		tags.value = []
-
-		uploadIngredients.value.value = ''
 
 		console.log(json)
 	} catch (e) {
@@ -73,5 +118,20 @@ const send = async () => {
 <style scoped lang="scss">
 input {
 	display: block;
+}
+
+label {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: flex-start;
+
+	p {
+		width: 100%;
+	}
+}
+
+hr {
+	margin-top: 10px;
+	margin-bottom: 10px;
 }
 </style>
