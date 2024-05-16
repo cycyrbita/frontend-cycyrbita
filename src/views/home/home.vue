fd0dcfd77fc723ed13f2e9dbc16ea4446e49814a
m.gromov <cycyrbita@mail.ru>
2024-05-16 18:07:13 +0300

    add navigation to home view

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,8 +1,39 @@
 <template>
-  <section class="home">
-    <div class="container">
-      <h1>Home</h1>
-    </div>
+  <section class="home wrapper">
+    <nav v-if="storeAuth.auth && storeUser.user.role !== 'role.default'" class="home__nav">
+      <div v-for="item in viewsList" ref="nav__item" class="home__nav-item">
+        <router-link :to="item.link" class="home__nav-link"> {{ item.value }}</router-link>
+      </div>
+    </nav>
   </section>
-
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from "@/stores/auth";
+import { useUserStore } from "@/stores/user";
+
+const storeAuth = useAuthStore()
+const storeUser = useUserStore()
+
+const viewsList = [
+  { link: '/ingredients', value: 'Ингредиенты' },
+  { link: '/new_promo', value: 'Новые промо' },
+  { link: '/clean', value: 'Чистые' },
+  { link: '/avatars', value: 'Аватарки' }
+]
+
+const nav__item = ref([])
+const itemBackground = (opacity) => `rgba(92, 113, 188, ${opacity})`
+const part = (array, index) => Math.round((index / array) * 10) / 10 + 0.4
+
+onMounted(() => {
+  const { value: navList, value: { length: arrayLength } } = nav__item
+  navList.forEach((item, index) => item.style.background = itemBackground(part(arrayLength, index)))
+})
+
+</script>
+
+<style scoped lang="scss">
+@import "@/views/home/styles/home.scss";
+</style>
