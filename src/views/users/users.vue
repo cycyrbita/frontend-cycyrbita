620bae07c943999d3683a164171dcefb3b7c420b
nikita <nikitka.kuznetsov.1994@mail.ru>
2024-08-24 23:53:03 +0300

    стили для пользователей

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -1,61 +1,140 @@
 <template>
   <div class="users">
-    <div v-for="user in users">
+    <div class="container">
       <div class="users__body">
-        <div class="users__info">
-          <p>{{ user.email }}</p>
-          <label>
-            <span>Ник</span>
-            <input
-              v-model="user.nickName"
-              type="text"
-            />
-          </label>
-          <label>
-            <span>Имя</span>
-            <input
-              v-model="user.firstName"
-              type="text"
-            />
-          </label>
-          <label>
-            <span>Фамилия</span>
-            <input
-              v-model="user.lastName"
-              type="text"
-            />
-          </label>
-          <label>
-            <span>Возраст</span>
-            <input
-              v-model="user.age"
-              type="text"
-            />
-          </label>
-          <label>
-            <span>Блокировка аккаунта</span>
-            <input
-              v-model="user.accountDeleted"
-              type="checkbox"
-            />
-          </label>
-        </div>
-        <div class="users__roles">
-          <label v-for="elem in roles">
-            <span>{{ elem.name }}</span>
-            <input
-              v-model="user.roles"
-              :value="elem"
-              type="checkbox"
-            />
-          </label>
+        <div
+          class="card-user"
+          v-for="user in users"
+        >
+          <div class="card-user__header">
+            <div class="card-user__name">{{ user.nickName }}</div>
+            <div class="card-user__email">{{ user.email }}</div>
+          </div>
+          <div class="card-user__body">
+            <span
+              v-for="role in user.roles"
+              class="card-user__description"
+              >{{ role.name }},
+            </span>
+          </div>
+          <div class="card-user__update">
+            <div class="card-user__info">
+              <label class="card-user__label">
+                <p class="card-user__title">Имя</p>
+                <input
+                  v-model="user.nickName"
+                  type="text"
+                  class="card-user__field"
+                />
+              </label>
+              <label class="card-user__label">
+                <p class="card-user__title">E-mail</p>
+                <input
+                  v-model="user.email"
+                  type="text"
+                  class="card-user__field"
+                />
+              </label>
+              <label class="card-user__label">
+                <p class="card-user__title">Пароль</p>
+                <input
+                  v-model="user.newPassword"
+                  type="text"
+                  class="card-user__field"
+                />
+              </label>
+              <label class="card-user__label">
+                <span class="card-user__title">Блокировка аккаунта</span>
+                <input
+                  v-model="user.accountDeleted"
+                  class="card-user__checkbox"
+                  type="checkbox"
+                />
+              </label>
+            </div>
+            <div class="card-user__roles">
+              <span class="card-user__title">Роль</span>
+              <div class="card-user__roles-checkbox">
+                <label
+                  v-for="elem in roles"
+                  class="card-user__label card-user__label--checkbox"
+                >
+                  <input
+                    v-model="user.roles"
+                    :value="elem"
+                    type="checkbox"
+                    class="card-user__checkbox"
+                  />
+                  <span class="card-user__title">{{ elem.name }}</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="card-user__buttons">
+            <button @click="updateUser(user)">Сохранить</button>
+            <button @click="deleteUser(user)">Удалить</button>
+          </div>
         </div>
       </div>
-      <button @click="updateUser(user)">save</button>
-      <button @click="deleteUser(user)">delete</button>
     </div>
-    <hr />
   </div>
+
+  <!--    <div v-for="user in users">-->
+  <!--      <div class="users__body">-->
+  <!--        <div class="users__info">-->
+  <!--          <p>{{ user.email }}</p>-->
+  <!--          <label>-->
+  <!--            <span>Ник</span>-->
+  <!--            <input-->
+  <!--              v-model="user.nickName"-->
+  <!--              type="text"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--          <label>-->
+  <!--            <span>Имя</span>-->
+  <!--            <input-->
+  <!--              v-model="user.firstName"-->
+  <!--              type="text"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--          <label>-->
+  <!--            <span>Фамилия</span>-->
+  <!--            <input-->
+  <!--              v-model="user.lastName"-->
+  <!--              type="text"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--          <label>-->
+  <!--            <span>Возраст</span>-->
+  <!--            <input-->
+  <!--              v-model="user.age"-->
+  <!--              type="text"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--          <label>-->
+  <!--            <span>Блокировка аккаунта</span>-->
+  <!--            <input-->
+  <!--              v-model="user.accountDeleted"-->
+  <!--              type="checkbox"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--        </div>-->
+  <!--        <div class="users__roles">-->
+  <!--          <label v-for="elem in roles">-->
+  <!--            <span>{{ elem.name }}</span>-->
+  <!--            <input-->
+  <!--              v-model="user.roles"-->
+  <!--              :value="elem"-->
+  <!--              type="checkbox"-->
+  <!--            />-->
+  <!--          </label>-->
+  <!--        </div>-->
+  <!--      </div>-->
+  <!--      <button @click="updateUser(user)">save</button>-->
+  <!--      <button @click="deleteUser(user)">delete</button>-->
+  <!--    </div>-->
+  <!--    <hr />-->
+  <!--  </div>-->
 </template>
 
 <script setup>
