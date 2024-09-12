542e00e3a181d3b3383ec9daea45b8fc36e6c1f9
nikita <nikitka.kuznetsov.1994@mail.ru>
2024-09-12 19:52:16 +0300

    дизайн доступов

--- src/views/permissions/permissions.vue
+++ src/views/permissions/permissions.vue
@@ -1,26 +1,68 @@
 <template>
-  <div class="roles">
-    <ul>
-      <li v-for="element in permissions">
-        <input
-          v-model="element.name"
-          type="text"
-        />
-        <button @click="setPermission(element)">save</button>
-        <button @click="deletePermission(element)">delete</button>
-      </li>
-    </ul>
-    <input
-      v-model="permission.name"
-      type="text"
-    />
-    <button @click="setPermission(permission)">Добавить доступ</button>
+  <Toast />
+  <div class="permissions">
+    <div class="container">
+      <div class="permissions__body">
+        <div class="permissions__row">
+          <div class="permissions__col">
+            <input
+              v-model="permission.name"
+              type="text"
+              class="permissions__field permissions__field--white"
+              placeholder="Добавить доступ"
+            />
+          </div>
+          <div class="permissions__col">
+            <button
+              class="permissions__button"
+              @click="setPermission(permission)"
+            >
+              Добавить доступ
+            </button>
+          </div>
+        </div>
+        <div
+          class="permissions__row"
+          v-for="element in permissions"
+        >
+          <div class="permissions__col">
+            <input
+              v-model="element.name"
+              type="text"
+              class="permissions__field"
+            />
+          </div>
+          <div class="permissions__col">
+            <button
+              class="permissions__button"
+              @click="setPermission(element)"
+            >
+              Изменить
+            </button>
+          </div>
+          <div class="permissions__col">
+            <button
+              class="permissions__button permissions__button--danger"
+              @click="deletePermission(element)"
+            >
+              Удалить
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
   import { onMounted, ref } from 'vue'
   import { useRolesStore } from '@/stores/roles'
+  import { useToast } from 'primevue/usetoast'
+  import Toast from 'primevue/toast'
+
+  const toast = useToast()
+
+  const statusCode = (detail = 'Успешно', severity = 'success', life = 3000) => toast.add({ severity: severity, detail: detail, life: life })
 
   const store = useRolesStore()
   const permission = ref({ name: '' })
@@ -41,8 +83,11 @@
       if (res.status === 200) {
         await getPermissions()
         permission.value.name = ''
+        const json = await res.json()
+        json?.statusInfo === 'update' ? statusCode('Доступ изменен') : statusCode('Доступ создан')
       }
     } catch (error) {
+      statusCode('Ошибка', 'error')
       console.log(error)
     }
   }
@@ -51,6 +96,7 @@
     try {
       const res = await store.deletePermission(permission)
       if (res.status === 200) await getPermissions()
+      statusCode('Доступ удален')
     } catch (error) {
       console.log(error)
     }
@@ -59,4 +105,6 @@
   onMounted(getPermissions)
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  @import '@/views/permissions/styles/permissions.scss';
+</style>
