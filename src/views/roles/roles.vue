c547b0dcc0f13e2996badb5f9c1d115aaeb612e4
nikita <nikitka.kuznetsov.1994@mail.ru>
2024-09-24 00:47:35 +0300

    роли и доступы

--- src/views/roles/roles.vue
+++ src/views/roles/roles.vue
@@ -1,58 +1,85 @@
 <template>
-	<div class="roles">
-		<div class="container">
-			<div class="roles__body">
-				<div class="roles__row">
-					<div class="roles__col roles__col--create-field">
-						<input
-								v-model="role.name"
-								type="text"
-								class="roles__field roles__field--white"
-								placeholder="Добавить роль"
-						/>
-					</div>
-					<div class="roles__col roles__col--create">
-						<button class="roles__button" @click="setRole(role)">
-							Добавить роль
-						</button>
-					</div>
-				</div>
-				<div class="roles__row" v-for="(element, index) in roles">
-					<div class="roles__index">{{ ++index }}.</div>
-					<div class="roles__col roles__col--first">
-						<input type="text" class="roles__field" v-model="element.name">
-					</div>
-					<div class="roles__col">
-						<button class="roles__button roles__button--save" @click="setRole(element)">Сохранить</button>
-					</div>
-					<div class="roles__col">
-						<button class="roles__button roles__button--delete roles__button--danger" @click="deleteRole(element)">Удалить</button>
-					</div>
-					<div class="roles__col roles__col--last">
-						<Transition name="bounce">
-							<MultiSelect
-									v-model="element.permissions"
-									class="roles__permissions"
-									panelClass="roles__permissions-panel"
-									:maxSelectedLabels="1"
-									:options="permissions"
-									optionLabel="name"
-									scrollHeight="700px"
-									:showToggleAll="false"
-									placeholder="Доступы"
-							/>
-						</Transition>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <Toast />
+  <div class="roles">
+    <div class="container">
+      <div class="roles__body">
+        <div class="roles__row">
+          <div class="roles__col roles__col--create-field">
+            <input
+              v-model="role.name"
+              type="text"
+              class="roles__field roles__field--white"
+              placeholder="Добавить роль"
+            />
+          </div>
+          <div class="roles__col roles__col--create">
+            <button
+              class="roles__button"
+              @click="setRole(role)"
+            >
+              Добавить роль
+            </button>
+          </div>
+        </div>
+        <div
+          class="roles__row"
+          v-for="(element, index) in roles"
+        >
+          <div class="roles__index">{{ ++index }}.</div>
+          <div class="roles__col roles__col--first">
+            <input
+              type="text"
+              class="roles__field"
+              v-model="element.name"
+            />
+          </div>
+          <div class="roles__col">
+            <button
+              class="roles__button roles__button--save"
+              @click="setRole(element)"
+            >
+              Сохранить
+            </button>
+          </div>
+          <div class="roles__col">
+            <button
+              class="roles__button roles__button--delete roles__button--danger"
+              @click="deleteRole(element)"
+            >
+              Удалить
+            </button>
+          </div>
+          <div class="roles__col roles__col--last">
+            <Transition name="bounce">
+              <MultiSelect
+                v-model="element.permissions"
+                class="roles__permissions"
+                panelClass="roles__permissions-panel"
+                :maxSelectedLabels="1"
+                :options="permissions"
+                optionLabel="name"
+                scrollHeight="700px"
+                :showToggleAll="false"
+                placeholder="Доступы"
+              />
+            </Transition>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
   import { onMounted, ref } from 'vue'
   import { useRolesStore } from '@/stores/roles'
-  import MultiSelect from "primevue/multiselect";
+  import MultiSelect from 'primevue/multiselect'
+  import Toast from 'primevue/toast'
+  import { useToast } from 'primevue/usetoast'
+
+  const toast = useToast()
+
+  const statusCode = (detail = 'Успешно', severity = 'success', life = 3000) => toast.add({ severity: severity, detail: detail, life: life })
 
   const store = useRolesStore()
   const role = ref({ name: '' })
@@ -76,9 +103,12 @@
       if (res.status === 200) {
         await getRoles()
         role.value.name = ''
+        const json = await res.json()
+        json?.statusInfo === 'update' ? statusCode('Роль изменилась') : statusCode('Роль создана')
       }
     } catch (error) {
       console.log(error)
+      statusCode('Ошибка', 'error')
     }
   }
 
@@ -87,8 +117,10 @@
     try {
       const res = await store.deleteRole(role)
       if (res.status === 200) await getRoles()
+      statusCode('Роль удалена')
     } catch (error) {
       console.log(error)
+      statusCode('Ошибка', 'error')
     }
   }
 
@@ -106,5 +138,5 @@
 </script>
 
 <style lang="scss">
-	@import "@/views/roles/styles/roles.scss";
+  @import '@/views/roles/styles/roles.scss';
 </style>
--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -1,78 +1,109 @@
 <template>
+  <Toast />
   <div class="users">
     <div class="container">
       <div class="users__body">
-        <div
-          class="card-user"
-          v-for="user in users"
-        >
-          <div class="card-user__header">
-            <div class="card-user__name">{{ user.nickName }}</div>
-            <div class="card-user__email">{{ user.email }}</div>
-          </div>
-          <div class="card-user__body">
-            <span
-              v-for="role in user.roles"
-              class="card-user__description"
-              >{{ role.name }},
-            </span>
-          </div>
-          <div class="card-user__update">
-            <div class="card-user__info">
-              <label class="card-user__label">
-                <p class="card-user__title">Имя</p>
-                <input
-                  v-model="user.nickName"
-                  type="text"
-                  class="card-user__field"
-                />
-              </label>
-              <label class="card-user__label">
-                <p class="card-user__title">E-mail</p>
-                <input
-                  v-model="user.email"
-                  type="text"
-                  class="card-user__field"
-                />
-              </label>
-              <label class="card-user__label">
-                <p class="card-user__title">Пароль</p>
-                <input
-                  v-model="user.newPassword"
-                  type="text"
-                  class="card-user__field"
-                />
-              </label>
-              <label class="card-user__label">
-                <span class="card-user__title">Блокировка аккаунта</span>
-                <input
-                  v-model="user.accountDeleted"
-                  class="card-user__checkbox"
-                  type="checkbox"
-                />
-              </label>
+        <div class="users__list">
+          <div
+            class="users-card"
+            v-for="user in users"
+            :key="user._id"
+          >
+            <div class="users-card__header">
+              <div class="users-card__name">{{ user.firstName }}</div>
+              <div class="users-card__mail">{{ user.email }}</div>
             </div>
-            <div class="card-user__roles">
-              <span class="card-user__title">Роль</span>
-              <div class="card-user__roles-checkbox">
-                <label
-                  v-for="elem in roles"
-                  class="card-user__label card-user__label--checkbox"
-                >
-                  <input
-                    v-model="user.roles"
-                    :value="elem"
-                    type="checkbox"
-                    class="card-user__checkbox"
-                  />
-                  <span class="card-user__title">{{ elem.name }}</span>
-                </label>
+            <div class="users-card__roles">
+              <div
+                class="users-card__role"
+                v-for="role in user.roles"
+                :key="role._id"
+              >
+                {{ role.name }}
               </div>
             </div>
-          </div>
-          <div class="card-user__buttons">
-            <button @click="updateUser(user)">Сохранить</button>
-            <button @click="deleteUser(user)">Удалить</button>
+            <div class="users-card__line"></div>
+            <div class="users-card__body">
+              <label
+                for=""
+                class="users-card__label"
+              >
+                <span class="users-card__label-name">Псевдоним</span>
+                <input
+                  type="text"
+                  class="users-card__input"
+                  v-model="user.nickName"
+                />
+              </label>
+              <label
+                for=""
+                class="users-card__label"
+              >
+                <span class="users-card__label-name">E-mail</span>
+                <input
+                  type="text"
+                  class="users-card__input"
+                  v-model="user.email"
+                />
+              </label>
+              <label
+                for=""
+                class="users-card__label"
+              >
+                <span class="users-card__label-name">Пароль</span>
+                <input
+                  type="text"
+                  class="users-card__input"
+                  v-model="user.newPassword"
+                />
+              </label>
+              <label
+                for=""
+                class="users-card__label"
+              >
+                <Transition name="bounce">
+                  <MultiSelect
+                    v-model="user.roles"
+                    :options="roles"
+                    class="users-card__select"
+                    panelClass="users-card__select-panel"
+                    :maxSelectedLabels="1"
+                    optionLabel="name"
+                    scrollHeight="700px"
+                    :showToggleAll="false"
+                    placeholder="Роли"
+                  />
+                </Transition>
+              </label>
+            </div>
+            <div class="users-card__block">
+              <Checkbox
+                v-model="user.accountDeleted"
+                :binary="true"
+                :inputId="`${user._id}-user-block`"
+                class="users-card__block-input"
+              />
+              <label
+                :for="`${user._id}-user-block`"
+                class="users-card__block-label"
+              >
+                <span class="users-card__block-label-name">Блокировка Аккаунта</span>
+              </label>
+            </div>
+            <div class="users-card__buttons">
+              <button
+                @click="updateUser(user)"
+                class="users-card__button users-card__button--save"
+              >
+                Сохранить
+              </button>
+              <button
+                @click="deleteUser(user)"
+                class="users-card__button users-card__button--danger"
+              >
+                Удалить
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -81,15 +112,23 @@
 </template>
 
 <script setup>
-  import { onMounted, ref } from 'vue'
+  import { onBeforeMount, ref } from 'vue'
   import { useUserStore } from '@/stores/user'
   import { useRolesStore } from '@/stores/roles'
+  import MultiSelect from 'primevue/multiselect'
+  import Checkbox from 'primevue/checkbox'
+  import Toast from 'primevue/toast'
+  import { useToast } from 'primevue/usetoast'
+
+  const toast = useToast()
 
   const store = useUserStore()
   const store_roles = useRolesStore()
 
   const users = ref()
   const roles = ref()
+
+  const statusCode = (detail = 'Успешно', severity = 'success', life = 3000) => toast.add({ severity: severity, detail: detail, life: life })
 
   const getUsers = async () => {
     try {
@@ -107,7 +146,9 @@
         .flat()
         .filter((el, i, arr) => i === arr.findIndex(el2 => el2.name === el.name))
       await store.updateUser(user)
+      statusCode('Пользователь обновлен')
     } catch (e) {
+      statusCode('Ошибка', 'error')
       console.log(e)
     }
   }
@@ -116,7 +157,9 @@
     try {
       const res = await store.deleteUser(user)
       if (res.status === 200) await getUsers()
+      statusCode('Пользователь удален')
     } catch (e) {
+      statusCode('Ошибка', 'error')
       console.log(e)
     }
   }
@@ -130,10 +173,10 @@
     }
   }
 
-  onMounted(getUsers)
-  onMounted(getRoles)
+  onBeforeMount(getUsers)
+  onBeforeMount(getRoles)
 </script>
 
-<style scoped>
+<style>
   @import '/src/views/users/styles/users.scss';
 </style>
